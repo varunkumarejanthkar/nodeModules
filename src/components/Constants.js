db0fa0000ed144750fbc83330c52a015df9f66cd
varunkumarejanthkar <78836546+varunkumarejanthkar@users.noreply.github.com>
2021-04-26 22:12:46 -0700

    Update Constants.js

--- src/components/Constants.js
+++ src/components/Constants.js
@@ -1,3 +1,3 @@
-export const url = "http://localhost:3001";
-//export const url = "http://3.129.18.147:3001";
+//export const url = "http://localhost:3001";
+export const url = "http://3.18.108.165:3001";
 
